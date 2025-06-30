e38351f3db009b48f7be2c8a23502451028495c5
theycallmefreire <guilherme120v@gmail.com>
2025-06-30 09:52:01 -0300

    melhorias

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from rembg import remove
 from PIL import Image
 import io
+import datetime
+
+img = (input("qual imagem deseja remover o fundo?"))
 
 # Abrir imagem original
-with open('matue.jpg', 'rb') as input_file:
+with open( img , 'rb') as input_file:
     input_data = input_file.read()
 
 # Remover fundo
 output_data = remove(input_data)
 
+# Gerar nome único com base na data/hora atual
+agora = datetime.datetime.now()
+nome_saida = f"imagem-sem-fundo_{agora.strftime('%Y%m%d_%H%M%S')}.png"
+
 # Salvar imagem com fundo removido
-with open('matue.png', 'wb') as output_file:
+with open(nome_saida, 'wb') as output_file:
     output_file.write(output_data)
 
 # Abrir imagem com fundo removido para verificar
