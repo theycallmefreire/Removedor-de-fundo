2365cda9e4fbf9c0a122bfb48966dc5288dc4d2e
theycallmefreire <guilherme120v@gmail.com>
2025-07-22 15:46:46 -0300

    melhorias na usabilidade

--- main.py
+++ main.py
@@ -2,25 +2,35 @@ from rembg import remove
 from PIL import Image
 import io
 import datetime
+from tkinter import Tk, filedialog
 
-img = (input("qual imagem deseja remover o fundo?"))
 
-# Abrir imagem original
-with open( img , 'rb') as input_file:
-    input_data = input_file.read()
+while True:
+    yes_or_not = input("Deseja remover o fundo de uma imagem? (sim/nao): ").strip().lower()
+    if yes_or_not != "sim":
+        print('finalizando aplicação... até mais!')
+        break
 
-# Remover fundo
-output_data = remove(input_data)
+    # Solicitar ao usuário o caminho da imagem
+    Tk().withdraw()  # Oculta a janelinha principal
+    img = filedialog.askopenfilename(title="Selecione a imagem")
 
-# Gerar nome único com base na data/hora atual
-agora = datetime.datetime.now()
-nome_saida = f"imagem-sem-fundo_{agora.strftime('%Y%m%d_%H%M%S')}.png"
+    # Abrir imagem original, le e quanda a informação na variavel 'input_data'
+    with open( img , 'rb') as input_file:
+        input_data = input_file.read()
 
-# Salvar imagem com fundo removido
-with open(nome_saida, 'wb') as output_file:
-    output_file.write(output_data)
+    # Remover fundo da imagem
+    output_data = remove(input_data)
 
-# Abrir imagem com fundo removido para verificar
-output_image = Image.open('matue.png')
-output_image.show()
-print("Fundo removido com sucesso!")
+    # Gerar nome único com base na data/hora atual
+    agora = datetime.datetime.now()
+    nome_saida = f"imagem-sem-fundo_{agora.strftime('%Y%m%d_%H%M%S')}.png"
+
+    # Salvar imagem com fundo removido
+    with open(nome_saida, 'wb') as output_file:
+        output_file.write(output_data)
+
+    # Abrir imagem com fundo removido para verificar
+    output_image = Image.open(nome_saida)
+    output_image.show()
+    print("Fundo removido com sucesso!")
